ca08069201d24587731425bf9c7ba231468f2f06
Ketan Saptasagare <ketansaptasagare3210@gmail.com>
2022-12-03 00:37:30 +0530

    Number Guessing Game
    A simple number guessing game, limited to just 5 tries.

---
+++ day3.py
@@ -0,0 +1,30 @@
+#number guessing game
+
+#importing the randint method from random module to generate a random integer
+from random import randint
+
+#method to run the guessing game, the no of tries are taken from the user
+def guess():    
+    i = 0
+    print(f"You have 5 guesses.\n")
+    print("Guess the mystery number(0-100).")
+    n = int(input("Number: "))
+    rand = randint(1,100)
+    while(i<4):
+        #giving the user some hints to reduces the attempts
+        if(n<rand):
+            print("\nYour guess is smaller than the mystery number.")
+            n = int(input("Enter again: "))
+            i+=1
+        elif(n>rand):
+            print("\nYour guess is greater than the mystery number.")
+            n = int(input("Enter again: "))
+            i+=1  
+        else:
+            print("\nYou guessed the mystery number. It was",rand)
+            break
+    else:
+        print("\nYou have failed to guess the mystery number. It was",rand)
+        
+#calling the method
+guess()
