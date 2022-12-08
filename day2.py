5492ae609cb10dac9386440b4eaad2daf8976150
Ketan Saptasagare <ketansaptasagare3210@gmail.com>
2022-12-09 01:30:04 +0530

    Age Calculator - Years (Updated)
    Updated a few parameters of the method.

--- day2.py
+++ day2.py
@@ -12,12 +12,12 @@ class agecalc:
         self.y = y
     
     #method to calculate the age in years
-    def age_y(self,d,m,y):
+    def age_y(self):
         super()
         # using now() to get current time
         today = datetime.datetime.now().date()
         #converting user input to date format
-        dob = datetime.date(y, m ,d)
+        dob = datetime.date(self.y, self.m, self.d)
         age = int((today-dob).days/365.25)
         return age
 
@@ -29,6 +29,6 @@ d = int(input("Day: "))
 
 #creating an object of class and calling the method
 obj = agecalc(d,m,y)
-age = obj.age_y(d,m,y)
+age = obj.age_y()
 print("The date of birth is {0:0}-{1:0}-{2:2}".format(d,m,y))
 print("The age is {0:3}".format(age))
